0552934e09aff3c090b6f088c6f71e67cb3697a2
Johnny <56370991+JuwuPu@users.noreply.github.com>
2020-11-23 23:39:54 +0800

    Update day 1.py

--- day 1.py
+++ day 1.py
@@ -1,1 +1,26 @@
-print('hello world')
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+class Solution:
+    def twoSum(self, nums, target):
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[num] = i
+            print(hashtable)
+        return []
+
+nums = [2, 4, 11,2, 15]
+target = 4
+print(Solution().twoSum(nums,target))
